baf6ba42909696cd2f0e24a4f2f2188117a994c1
gnayoaixgnaw <71043501+gnayoaixgnaw@users.noreply.github.com>
2021-01-13 06:11:06 -0500

    Create 3sum_closest.py

---
+++ 3sum_closest.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        dis = 1000000
+        res = 0
+        for i in range(len(nums)-2):
+            left = i+1
+            right = len(nums) - 1
+            while left < right :
+                sums = nums[i]+nums[left] +nums[right]
+                if abs(sums - target) <= dis :
+                    dis = abs(sums - target)
+                    res = sums
+                    if (sums -target) > 0:
+                        right -=1
+                    elif (sums - target) <0:
+                        left +=1
+                    else:
+                        return res
+                else:
+                    if (sums -target) > 0:
+                        right -=1
+                    elif (sums - target) <0:
+                        left +=1
+                    else:
+                        return res
+        return res
+            

0e00beffc0027cdb571af4ffe1abdf41d1cc94fb
gnayoaixgnaw <71043501+gnayoaixgnaw@users.noreply.github.com>
2021-01-13 06:36:49 -0500

    Create letter_combination_of_phonenumber.py

---
+++ letter_combination_of_phonenumber.py
@@ -0,0 +1,18 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        dic = {'1':[],'2':['a','b','c'], '3':['d','e','f'],'4':['g','h','i'],'5':['j','k','l'],'6':['m','n','o'],
+              '7':['p','q','r','s'],'8':['t','u','v'],'9':['w','x','y','z']}
+        res = ['']
+        temp = []
+        if len(digits) == 0:
+            return []
+        else:
+            
+            while len(digits)!= 0:
+                for i in dic[digits[-1]]:
+                    for j in res:
+                        temp.append(i+j)
+                res = temp
+                temp = []
+                digits = digits[:-1]
+            return res
